d9fdfcf3864427eca4b57d3ecd1fee854fb9ea47
Courage Zeng <czeng@lcs.on.ca>
2026-05-20 09:23:50 -0400

    Initial Commit

---
+++ MindSweeper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MindSweeper
+//
+//  Created by 曾梓恒 on 5/20/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MindSweeper/MindSweeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindSweeperApp.swift
+//  MindSweeper
+//
+//  Created by 曾梓恒 on 5/20/26.
+//
+
+import SwiftUI
+
+@main
+struct MindSweeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
